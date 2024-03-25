4f5ef725d8b6438064b6ebb3a3e2bd906631004b
alessandroraucci <148247466+alessandroraucci@users.noreply.github.com>
2024-03-25 13:53:15 +0100

    Loading Unity Project

---
+++ NCX - Endless Runner/Assets/Script/Loop.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Loop : MonoBehaviour
+{
+    void Update()
+    {
+        transform.position -= new Vector3(0.05f, 0, 0);
+        if(transform.position.x <= -40.0025f)
+        {
+            Instantiate(gameObject, new Vector3(120.01f, 0, 0), Quaternion.identity);
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ NCX - Endless Runner/Assets/Script/TestingInputSystem.cs
@@ -0,0 +1,57 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestingInputSystem : MonoBehaviour
+{
+    private Rigidbody characterRigidbody;
+    public float moveSpeed = 5f;
+    public bool canJump;
+
+    private void Update()
+    {
+        Move();
+    }
+
+    private void Awake()
+    {
+        characterRigidbody = GetComponent<Rigidbody>();
+
+    }
+    public void Jump()
+    {
+        if (canJump)
+        {
+            characterRigidbody.AddForce(Vector3.up * 25f, ForceMode.Impulse);
+            canJump = false;
+
+        }
+    }
+
+    private void Move()
+    {
+        float horizontalInput = Input.GetAxis("Horizontal");
+        float verticalInput = Input.GetAxis("Vertical");
+
+        if(!(transform.position.x + verticalInput > -0.35 || transform.position.x + verticalInput < 0.35))
+        {
+            verticalInput = 0;
+            
+        }
+
+        if (!(transform.position.y + horizontalInput > -0.6 || transform.position.y + horizontalInput < 0.6))
+        {
+            horizontalInput = 0;
+
+        }
+
+        Vector3 movement = new Vector3(verticalInput, 0f, -horizontalInput) * moveSpeed * Time.deltaTime;
+        characterRigidbody.MovePosition(transform.position + movement);
+
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        canJump = true;
+    }
+}
