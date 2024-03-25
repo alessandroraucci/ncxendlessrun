653f558dd4a2cb7d8056264227ece220039cd00a
alessandroraucci <148247466+alessandroraucci@users.noreply.github.com>
2024-03-25 15:06:12 +0100

    Jump e limitazione corsia

--- NCX - Endless Runner/Assets/Script/TestingInputSystem.cs
+++ NCX - Endless Runner/Assets/Script/TestingInputSystem.cs
@@ -31,22 +31,22 @@ public class TestingInputSystem : MonoBehaviour
     private void Move()
     {
         float horizontalInput = Input.GetAxis("Horizontal");
-        float verticalInput = Input.GetAxis("Vertical");
+        //float verticalInput = Input.GetAxis("Vertical");
 
-        if(!(transform.position.x + verticalInput > -0.35 || transform.position.x + verticalInput < 0.35))
+        /*if ((transform.position.x + verticalInput > 4.50f || transform.position.x + verticalInput < -4.50f))
         {
             verticalInput = 0;
-            
-        }
+        }*/
 
-        if (!(transform.position.y + horizontalInput > -0.6 || transform.position.y + horizontalInput < 0.6))
+        if ((transform.position.z - horizontalInput < -4.45f || transform.position.z - horizontalInput > 3.65f))
         {
             horizontalInput = 0;
-
         }
 
-        Vector3 movement = new Vector3(verticalInput, 0f, -horizontalInput) * moveSpeed * Time.deltaTime;
+
+        Vector3 movement = new Vector3(0f, 0f, -horizontalInput) * moveSpeed * Time.deltaTime;
         characterRigidbody.MovePosition(transform.position + movement);
+        //characterRigidbody.MovePosition(Vector3.Lerp(transform.position, transform.position + movement, 1));
 
     }
 
